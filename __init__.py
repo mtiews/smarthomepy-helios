e17e3b997100f27d85f5814cd102bfdb1cbacd48
Marcel Tiews <marcel.tiews@googlemail.com>
2014-09-15 22:27:45 +0200

    Updated

--- __init__.py
+++ __init__.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python
-# vim: set encoding=utf-8 tabstop=4 softtabstop=4 shiftwidth=4 expandtab
 #########################################################################
-# Copyright 2012-2013 KNX-User-Forum e.V.       http://knx-user-forum.de/
+# Copyright 2014 Marcel Tiews marcel.tiews@gmail.com
 #########################################################################
-#  This file is part of SmartHome.py.   http://smarthome.sourceforge.net/
+# Helios-Plugin for SmartHome.py. http://mknx.github.io/smarthome/
 #
-#  SmartHome.py is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
+# This plugin is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#  SmartHome.py is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#  GNU General Public License for more details.
+# This plugin is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 #
-#  You should have received a copy of the GNU General Public License
-#  along with SmartHome.py. If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License
+# along with this plugin. If not, see <http://www.gnu.org/licenses/>.
 #########################################################################
 
 import sys
